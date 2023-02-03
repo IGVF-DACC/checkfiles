2a5450913a4eaeabd29a145415b49631b257a389
Mingjie Li <mingjiecn@gmail.com>
2023-02-03 15:22:36 -0600

    validate fastq using validateFiles

--- src/checkfiles/checkfiles.py
+++ src/checkfiles/checkfiles.py
@@ -67,6 +67,7 @@ VALIDATE_FILES_ARGS = {
     ('bigBed', 'bed3+'): ['-tab', '-type=bigBed3+'],
     ('bigWig', None): ['-type=bigWig'],
     ('bigInteract', None): ['-type=bigBed5+13', '-as=src/schemas/file_formats/as/interact.as'],
+    ('fastq', None): ['-type=fastq'],
 
 }
 
@@ -116,6 +117,8 @@ def file_validation(bucket_name, key, uuid, md5sum, file_format, output_type, fi
         if 'bam_error' not in errors:
             bam_generate_bai_file(file_path)
     elif file_format == 'fastq':
+        error = validate_files_fastq_check(file_path)
+        errors.update(error)
         error = fastq_check(file_path, number_of_reads, read_length)
         errors.update(error)
     elif file_format in ['bed', 'bigWig', 'bigInteract', 'bigBed', 'bedpe']:
@@ -278,6 +281,18 @@ def validate_files_check(file_path, file_format, file_format_type, assembly):
     except subprocess.CalledProcessError as e:
         error['validate_files'] = e.output.decode(
             errors='replace').rstrip('\n')
+    return error
+
+
+def validate_files_fastq_check(file_path):
+    error = {}
+    command = ['validateFiles'] + ['-type=fastq'] + [file_path]
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        error['validate_files'] = e.output.decode(
+            errors='replace').rstrip('\n')
+
     return error
 
 
