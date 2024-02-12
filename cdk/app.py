a41b1713db4a52c6c883a2992c656f956f8e932b
Otto Jolanki <ojolanki@stanford.edu>
2024-02-12 11:38:00 -0800

    yeah, search and replace

--- cdk/app.py
+++ cdk/app.py
@@ -36,7 +36,7 @@ production_props = RunCheckfilesStepFunctionProps(
     instance_name=config['instance_name_production'],
     instance_profile_arn=config['instance_profile_arn_production'],
     instance_security_group_id=config['instance_security_group_production'],
-    checkfiles_tag=config['checkfiles_branch_production'],
+    checkfiles_tag=config['checkfiles_tag_production'],
     portal_secrets_arn=config['portal_secrets_arn_production'],
     backend_uri=config['backend_uri_production'],
 )
