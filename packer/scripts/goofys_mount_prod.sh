b6900a50f714df30467fe2fd7bbd0d3ef06f3cf0
ottojolanki <ojolanki@stanford.edu>
2024-06-07 15:50:20 -0700

    CHECK-89-controlled-access-infra(#71)

--- packer/scripts/goofys_mount_prod.sh
+++ packer/scripts/goofys_mount_prod.sh
@@ -4,3 +4,8 @@ sudo mkdir /home/ubuntu/igvf-files
 sudo chmod 755 /home/ubuntu/igvf-files
 
 sudo echo "goofys#igvf-files   /home/ubuntu/igvf-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
+
+sudo mkdir /home/ubuntu/igvf-restricted-files
+sudo chmod 755 /home/ubuntu/igvf-restricted-files
+
+sudo echo "goofys#igvf-restricted-files   /home/ubuntu/igvf-restricted-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
