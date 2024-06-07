0afb8b0c550eb938ca16cf82b4fd2de3b16f7c97
Otto Jolanki <ojolanki@stanford.edu>
2024-06-07 15:43:45 -0700

    lambda and ami building

--- cdk/checkfiles_runner/lambdas/check_pending/main.py
+++ cdk/checkfiles_runner/lambdas/check_pending/main.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 )
 
 
-PENDING_FILES_SEARCH = '/search?type=File&upload_status=pending&controlled_access=false'
+PENDING_FILES_SEARCH = '/search?type=File&upload_status=pending'
 
 
 def get_secret_arn():
