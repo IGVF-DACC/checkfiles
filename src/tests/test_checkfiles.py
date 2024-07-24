5797bac244b1b6a55b340a6562f410057bbd194b
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2024-07-24 17:35:41 -0500

    IGVF-1827-content-type update content type for MPRA sequence designs file (#81)

--- src/tests/test_checkfiles.py
+++ src/tests/test_checkfiles.py
@@ -167,15 +167,15 @@ def test_tabular_file_check_guide_rna_sequences_invalid():
     }
 
 
-def test_tabular_file_check_designed_sequences_valid():
-    file_path = 'src/tests/data/designed_sequences_valid.tsv'
-    error = tabular_file_check('designed sequences', file_path)
+def test_tabular_file_check_mpra_sequence_designs_valid():
+    file_path = 'src/tests/data/mpra_sequence_designs_valid.tsv'
+    error = tabular_file_check('MPRA sequence designs', file_path)
     assert error == {}
 
 
-def test_tabular_file_check_designed_sequences_invalid():
-    file_path = 'src/tests/data/designed_sequences_invalid.tsv'
-    error = tabular_file_check('designed sequences', file_path)
+def test_tabular_file_check_mpra_sequence_designs_invalid():
+    file_path = 'src/tests/data/mpra_sequence_designs_invalid.tsv'
+    error = tabular_file_check('MPRA sequence designs', file_path)
     assert error == {
         'tabular_file_error': [
             [6, 1, 'constraint-error', 'constraint "required" is "True"'],
