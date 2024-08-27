f496ba83e80d7bcf50af677a1b33e1a1f03eef2d
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2024-08-27 11:58:26 -0500

    CHECK-102-local (#84)

---
+++ src/checkfiles/checkfiles_local.py
@@ -0,0 +1,118 @@
+import argparse
+import logging
+import file
+import logformatter
+from pprint import pprint
+
+from checkfiles import TABULAR_FORMAT
+from checkfiles import check_valid_gzipped_file_format, check_md5sum, bam_pysam_check, fastq_get_average_read_length_and_number_of_reads, fasta_check, tabular_file_check, get_validate_files_args, validate_files_check, validate_files_fastq_check
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(logformatter.JsonFormatter())
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def file_validation(input_file_path, validation_record: file.FileValidationRecord, submitted_md5sum, content_type, file_format_type, assembly, tabular_file_schema_path):
+    logger.info(f'Checking file: {input_file_path}')
+    try:
+        true_file_size_bytes = validation_record.file.size
+        validation_record.update_info({'file_size': true_file_size_bytes})
+        if true_file_size_bytes == 0:
+            validation_record.update_errors(
+                {'file_size': 'file has zero size'})
+            return validation_record
+    except FileNotFoundError:
+        logger.warning(f'File not found at path {input_file_path}')
+        validation_record.file_not_found = True
+        return validation_record
+    file_format = validation_record.file.file_format
+    is_gzipped = validation_record.file.is_zipped
+    gzipped_format_error = check_valid_gzipped_file_format(
+        is_gzipped, file_format)
+    validation_record.update_errors(gzipped_format_error)
+    md5_sum_error = check_md5sum(
+        submitted_md5sum, validation_record.file.md5sum)
+    validation_record.update_errors(md5_sum_error)
+    if file_format == 'bam':
+        bam_check_result = bam_pysam_check(input_file_path)
+        if 'bam_error' in bam_check_result:
+            validation_record.update_errors(bam_check_result)
+        else:
+            validation_record.update_info(bam_check_result)
+    elif file_format == 'fastq':
+        validate_files_fastq_check_error = validate_files_fastq_check(
+            input_file_path)
+        validation_record.update_errors(validate_files_fastq_check_error)
+        fastq_read_info = fastq_get_average_read_length_and_number_of_reads(
+            input_file_path)
+        validation_record.update_info(fastq_read_info)
+    elif file_format in ['bed', 'bigWig', 'bigInteract', 'bigBed', 'bedpe']:
+        validate_files_check_error = validate_files_check(
+            input_file_path, file_format, file_format_type, assembly)
+        validation_record.update_errors(validate_files_check_error)
+    elif file_format == 'fasta':
+        fasta_check_error = fasta_check(input_file_path, is_gzipped)
+        validation_record.update_errors(fasta_check_error)
+    elif file_format in TABULAR_FORMAT:
+        if not content_type and not tabular_file_schema_path:
+            logger.info(
+                'file content type and tabular file schema are not provided for the tabular file, will only perform tabular file based checks')
+        tabular_file_check_error = tabular_file_check(
+            content_type, input_file_path, schema_path=tabular_file_schema_path)
+        validation_record.update_errors(tabular_file_check_error)
+
+    if validation_record.errors:
+        validation_record.validation_success = False
+        return validation_record
+    else:
+        validation_record.validation_success = True
+        return validation_record
+
+
+def main(args):
+    if args.assembly is None and args.file_format in ['bed', 'bigWig', 'bigInteract', 'bigBed', 'bedpe']:
+        raise ValueError(
+            'assembly is required for file formats: bed, bigWig, bigInteract, bigBed, bedpe')
+    if args.file_format in ['bed', 'bigBed'] and args.file_format_type is None:
+        raise ValueError(
+            'file_format_type is required for bed and bigBed file')
+    file_validation_record = file.FileValidationRecord(
+        file.get_file(args.input_file_path, args.file_format))
+    file_validation_complete_record = file_validation(args.input_file_path, file_validation_record,
+                                                      args.md5sum, args.content_type, args.file_format_type, args.assembly, args.tabular_file_schema_path)
+    if not file_validation_complete_record.file_not_found:
+        if file_validation_complete_record.errors:
+            logger.info(
+                f'file validation is completed and errors are found.')
+        else:
+            logger.info('file validation is completed and no error found.')
+        print('\nFile info:')
+        pprint(file_validation_complete_record.info)
+        if file_validation_complete_record.errors:
+            print('\nFile errors:')
+            pprint(file_validation_complete_record.errors)
+
+
+# Start script
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Checkfiles argumentparser')
+    parser.add_argument('--input_file_path', required=True,
+                        help='path of the local file to be checked.')
+    # assembly is required for some file formats: 'bed', 'bigWig', 'bigInteract', 'bigBed', 'bedpe'
+    parser.add_argument(
+        '--assembly', choices=['GRCh38', 'GRCm39'], help='assembly of the file to be checked.')
+    parser.add_argument(
+        '--content_type', help='content type of the file to be checked.')
+    parser.add_argument('--file_format', choices=['bam', 'bed', 'bigWig', 'bigInteract', 'bigBed', 'bedpe',
+                        'csv', 'fasta', 'fastq', 'tsv', 'txt'], required=True, help='file format of the file to be checked.')
+    # file_format_type is required for bed file and bigBed file
+    parser.add_argument('--file_format_type',
+                        help='file format type of the file to be checked.')
+    parser.add_argument('--md5sum', help='md5sum of the file to be checked.')
+    parser.add_argument('--tabular_file_schema_path',
+                        help='the relative path to the schema file of the tabular file.')
+
+    args = parser.parse_args()
+    main(args)
---
+++ validate_file_locally.md
@@ -0,0 +1,72 @@
+# How to Validate Files Locally
+
+## Install Dependencies
+
+1. Create a python environment and install dependencies from src/checkfiles/requirements.txt
+
+    ```bash
+    pip install -r src/checkfiles/requirements.txt
+    ```
+
+2. Download the tool validateFiles for validating bed files. The file is hosted here: <https://hgdownload.cse.ucsc.edu/admin/exe/>. Choose the correct folder for different operating system. For example, for macOS arm64, the file is in this folder: <https://hgdownload.cse.ucsc.edu/admin/exe/macOSX.arm64/>. Run the following command for downloading:
+
+    ```bash
+    rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/macOSX.arm64/validateFiles /usr/local/bin/
+    sudo chmod 755 /usr/local/bin/validateFiles
+    ```
+
+3. Install Rust and Cargo. Here is the command to install them on mac:
+
+    ```bash
+    curl https://sh.rustup.rs -sSf | sh
+    ```
+
+4. Install fastq_stats that calculates couple of fastq stats. Here is how to install it on Mac:
+
+    ```bash
+    git clone https://github.com/IGVF-DACC/fastq_stats.git
+    cd fastq_stats
+    cargo build --release
+    sudo cp target/release/fastq_stats /usr/local/bin
+    sudo chmod 755 /usr/local/bin/fastq_stats
+    ```
+
+## Validate Files locally
+
+Here are some examples to show you how to validate files locally.
+
+Validating bam file:
+
+```bash
+python src/checkfiles/checkfiles_local.py --input_file_path src/tests/data/ENCFF206HGF.bam --file_format bam --md5sum 2d3b7df013d257c7052c084d93ff9026
+```
+
+Validating bed, bigWig, bigInteract, bigBed and bedpe file:
+
+```bash
+python src/checkfiles/checkfiles_local.py --input_file_path src/tests/data/ENCFF597JNC.bed.gz --file_format bed --file_format_type bed3 --assembly GRCh38 --md5sum d1bae8af8fec54424cff157134652d26
+```
+
+Validating fasta file:
+
+```bash
+python src/checkfiles/checkfiles_local.py --input_file_path src/tests/data/ENCFF329FTG.fasta.gz --file_format fasta --md5sum c8c18396efe2a44e93f613d00c00823d
+```
+
+Validating fastq file:
+
+```bash
+python src/checkfiles/checkfiles_local.py --input_file_path src/tests/data/ENCFF594AYI.fastq.gz --file_format fastq --md5sum 3e814f4af7a4c13460584b26fbe32dc4
+```
+
+Validating tabular file (csv, tsv and txt):
+
+```bash
+python src/checkfiles/checkfiles_local.py --input_file_path src/tests/data/guide_rna_sequences_invalid.tsv.gz --file_format tsv --content_type "guide RNA sequences" --md5sum b8bfdca28ddbcc74128e3e3bb5febe24
+```
+
+Validating tabular file with your own schema file:
+
+```bash
+python src/checkfiles/checkfiles_local.py --input_file_path src/tests/data/guide_rna_sequences_invalid.tsv.gz --file_format tsv --content_type "guide RNA sequences" --md5sum b8bfdca28ddbcc74128e3e3bb5febe24 --tabular_file_schema_path src/schemas/table_schemas/element_quant.json
+```
