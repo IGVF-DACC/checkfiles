5f88b2a9930661831fd78c6ab49c259eacc769f4
ottojolanki <ojolanki@stanford.edu>
2024-09-27 10:16:38 -0700

    v10-sandbox-v10-prod (#87)

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -15,5 +15,5 @@ config = {
     'backend_uri_sandbox': 'https://api.sandbox.igvf.org',
     'backend_uri_production': 'https://api.data.igvf.org',
     'checkfiles_tag_sandbox': 'v10',
-    'checkfiles_tag_production': 'v9',
+    'checkfiles_tag_production': 'v10',
 }
