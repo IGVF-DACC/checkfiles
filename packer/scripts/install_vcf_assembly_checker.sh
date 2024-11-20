2cf68728e7b2a800411870f39e78262e8c7fb4b2
Otto Jolanki <ojolanki@stanford.edu>
2024-11-20 09:36:21 -0800

    assembly checker script

--- packer/scripts/install_vcf_assembly_checker.sh
+++ packer/scripts/install_vcf_assembly_checker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 sudo apt-get install cmake wget build-essential libboost-all-dev -y
-sudo wget https://raw.githubusercontent.com/IGVF-DACC/vcfAssemblyChecker/main/vcf_assembly_checker
-chmod +x vcf_assembly_checker
+sudo wget https://github.com/EBIvariation/vcf-validator/releases/download/v0.10.0/vcf_assembly_checker_linux
+sudo chmod 755 vcf_assembly_checker_linux
+sudo cp vcf_assembly_checker_linux /usr/bin/vcf_assembly_checker
