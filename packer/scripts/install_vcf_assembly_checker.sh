c80b60ca0a675ca9352ce03e27ab131c3627a628
Mingjie Li <mingjiecn@gmail.com>
2024-11-22 15:02:58 -0600

    copy to /usr/bin

--- packer/scripts/install_vcf_assembly_checker.sh
+++ packer/scripts/install_vcf_assembly_checker.sh
@@ -3,4 +3,4 @@ set -ex
 sudo apt-get install cmake wget build-essential libboost-all-dev -y
 sudo wget https://github.com/EBIvariation/vcf-validator/releases/download/v0.10.0/vcf_assembly_checker_linux
 sudo chmod 755 vcf_assembly_checker_linux
-sudo cp vcf_assembly_checker_linux /usr/local/bin/vcf_assembly_checker
+sudo cp vcf_assembly_checker_linux /usr/bin/vcf_assembly_checker
