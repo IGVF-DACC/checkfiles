ebf592601ba141fc65a59387ad2ec53b96dbc013
ottojolanki <ojolanki@stanford.edu>
2025-03-03 10:52:44 -0800

    v17-sandbox-v17-prod(#124)

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -15,5 +15,5 @@ config = {
     'backend_uri_sandbox': 'https://api.sandbox.igvf.org',
     'backend_uri_production': 'https://api.data.igvf.org',
     'checkfiles_tag_sandbox': 'v17',
-    'checkfiles_tag_production': 'v16',
+    'checkfiles_tag_production': 'v17',
 }
