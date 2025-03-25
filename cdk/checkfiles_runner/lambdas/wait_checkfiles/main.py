1e528c2b354072d70c6194e3e7ee8deb38fb565c
Otto Jolanki <ojolanki@stanford.edu>
2025-03-25 15:16:12 -0700

    counter loop

--- cdk/checkfiles_runner/lambdas/wait_checkfiles/main.py
+++ cdk/checkfiles_runner/lambdas/wait_checkfiles/main.py
@@ -10,14 +10,11 @@ logging.basicConfig(
 )
 
 
-class CommandInProgress(Exception):
-    pass
-
-
-def wait_checkfiles_command_to_finish(event, context):
+def get_checkfiles_command_status(event, context):
     ssm = boto3.client('ssm')
     instance_id = event['instance_id']
     command_id = event['command_id']
+    iterator = event['iterator']
 
     result = ssm.get_command_invocation(
         CommandId=command_id,
@@ -26,16 +23,13 @@ def wait_checkfiles_command_to_finish(event, context):
 
     command_status = result['Status']
 
-    if command_status == 'InProgress':
-        logging.info(
-            f'Command {command_id} on instance {instance_id} In Progress.')
-        raise CommandInProgress
-    else:
-        logging.info(
-            f'Command {command_id} on instance {instance_id} in status {command_status}. Proceeding to terminate the instance')
-        return {
-            'instance_id': instance_id,
-            'checkfiles_command_status': command_status,
-            'command_id': command_id,
-            'instance_id_list': [instance_id]
-        }
+    logging.info(
+        f'Command {command_id} on instance {instance_id} in status {command_status}.')
+    return {
+        'instance_id': instance_id,
+        'checkfiles_command_status': command_status,
+        'command_id': command_id,
+        'instance_id_list': [instance_id],
+        'in_progress': command_status == 'InProgress',
+        'iterator': iterator
+    }
