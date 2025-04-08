b8dbdfa61434f0f11dfcd692d9e4c46a95cad57a
Idan <gabdank@stanford.edu>
2025-04-08 12:29:25 -0700

    updating packer

--- packer/README.md
+++ packer/README.md
@@ -6,6 +6,11 @@ To build new AMI:
 
 `*` In the `checkfiles_ubuntu_2204_variables.json` set `aws_profile_name` to correspond the name of the account you want to target. This name should match the account name in your `~/.aws/credentials` file.
 
+`*` Run:
+```bash
+packer plugins install github.com/hashicorp/amazon
+```
+
 `*` In `templates` directory, run:
 ```bash
 packer build --var-file="checkfiles_ubuntu_2204_variables.json" build_AMI.pkr.hcl
--- packer/scripts/goofys_mount.sh
+++ packer/scripts/goofys_mount.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -ex
-sudo mkdir /home/ubuntu/igvf-files
-sudo mkdir /home/ubuntu/igvf-files-staging
-sudo mkdir /home/ubuntu/igvf-restricted-files
-sudo mkdir /home/ubuntu/igvf-restricted-files-staging
-sudo chmod 755 /home/ubuntu/igvf-files
-sudo chmod 755 /home/ubuntu/igvf-files-staging
-sudo chmod 755 /home/ubuntu/igvf-restricted-files
-sudo chmod 755 /home/ubuntu/igvf-restricted-files-staging
+sudo mkdir /home/ubuntu/lattice-files
+sudo chmod 755 /home/ubuntu/lattice-files
 
-sudo echo "goofys#igvf-files   /home/ubuntu/igvf-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
-sudo echo "goofys#igvf-files-staging   /home/ubuntu/igvf-files-staging        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
-sudo echo "goofys#igvf-restricted-files   /home/ubuntu/igvf-restricted-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
-sudo echo "goofys#igvf-restricted-files-staging   /home/ubuntu/igvf-restricted-files-staging        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
+sudo echo "goofys#igvf-files   /home/ubuntu/lattice-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
