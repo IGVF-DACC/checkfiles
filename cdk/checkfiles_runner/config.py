da14ba53e0cd535f600cc46d78299071aacfe59f
Idan <gabdank@stanford.edu>
2025-04-09 13:37:16 -0700

    ami and security group

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -1,11 +1,11 @@
 config = {
     'region': 'us-west-1',
     'account_production': '585222078325',  # lattice-prod
-    'ami_id_production': 'ami-06e22e357d852a33a',
+    'ami_id_production': 'ami-0960bbd59c36f582d',
     'portal_secrets_arn_production': 'arn:aws:secretsmanager:us-west-1:585222078325:secret:checkfiles-portal-secret-5A1cNL',
     'instance_name_production': 'checkfiles-production',
     'instance_profile_arn_production': 'arn:aws:iam::585222078325:instance-profile/checkfiles-instance',
-    'instance_security_group_production': 'sg-b90e6acb',
+    'instance_security_group_production': 'sg-0da14ac5025210cf9',
     'backend_uri_production': 'https://www.lattice-data.org',
     'checkfiles_tag_production': 'v22',
 }
--- packer/scripts/goofys_mount.sh
+++ packer/scripts/goofys_mount.sh
@@ -3,4 +3,4 @@ set -ex
 sudo mkdir /home/ubuntu/lattice-files
 sudo chmod 755 /home/ubuntu/lattice-files
 
-sudo echo "goofys#igvf-files   /home/ubuntu/lattice-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
+sudo echo "goofys#submissions-czi013mog   /home/ubuntu/lattice-files        fuse     _netdev,allow_other,--file-mode=0666    0       0" | sudo tee -a /etc/fstab
