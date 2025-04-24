ccb34ccb5151d3f507cd6d12e234a663187e0b25
Mingjie Li <mingjiecn@gmail.com>
2025-04-24 14:07:47 -0500

    add script to install samtools

---
+++ packer/scripts/install_samtools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y libncurses-dev libbz2-dev liblzma-dev libcurl4-openssl-dev
+sudo wget https://github.com/samtools/samtools/releases/download/1.20/samtools-1.20.tar.bz2
+tar xvjf samtools-1.20.tar.bz2
+cd samtools-1.20/
+sudo make
+sudo make install
