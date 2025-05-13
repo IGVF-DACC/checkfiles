404787ab95941a0b8213daf811a4e97e303a59db
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2025-05-13 14:16:36 -0500

    v23-sandbox-v23-prod (#161)

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -15,5 +15,5 @@ config = {
     'backend_uri_sandbox': 'https://api.sandbox.igvf.org',
     'backend_uri_production': 'https://api.data.igvf.org',
     'checkfiles_tag_sandbox': 'v23',
-    'checkfiles_tag_production': 'v20',
+    'checkfiles_tag_production': 'v23',
 }
