ea879fa873da29ed9d5e879ec35c7a4d89c0f028
Mingjie Li <mingjiecn@gmail.com>
2025-08-27 11:33:37 -0500

    v31-sandbox-v31-prod

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -15,5 +15,5 @@ config = {
     'backend_uri_sandbox': 'https://api.sandbox.igvf.org',
     'backend_uri_production': 'https://api.data.igvf.org',
     'checkfiles_tag_sandbox': 'v31',
-    'checkfiles_tag_production': 'v30',
+    'checkfiles_tag_production': 'v31',
 }
