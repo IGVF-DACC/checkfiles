25b11d9b435b06c63e0070cf181b73ac3590c55e
Mingjie Li <mingjiecn@gmail.com>
2026-02-23 13:48:49 -0600

    v40-staging-v40-prod

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -15,5 +15,5 @@ config = {
     'backend_uri_sandbox': 'https://api.staging.igvf.org',
     'backend_uri_production': 'https://api.data.igvf.org',
     'checkfiles_tag_sandbox': 'v40',
-    'checkfiles_tag_production': 'v39',
+    'checkfiles_tag_production': 'v40',
 }
