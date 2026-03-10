aad3d61f0525c8ec4a05b5e842df9a253f24e02e
Mingjie Li <mingjiecn@gmail.com>
2026-03-10 16:31:37 -0500

    specify compression type for frictionless

--- src/checkfiles/checkfiles.py
+++ src/checkfiles/checkfiles.py
@@ -153,7 +153,7 @@ def file_validation(portal_url, portal_auth: PortalAuth, validation_record: file
         validation_record.update_errors(h5ad_check_error)
     elif file_format in TABULAR_FORMAT:
         tabular_file_check_error = tabular_file_check(
-            file_format, content_type, local_file_path, is_gzipped)
+            file_format, content_type, local_file_path)
         validation_record.update_errors(tabular_file_check_error)
     elif file_format in ['vcf', 'gvcf']:
         vcf_check_error = vcf_sequence_check(local_file_path, assembly)
@@ -175,9 +175,10 @@ def file_validation(portal_url, portal_auth: PortalAuth, validation_record: file
     return validation_record
 
 
-def get_header_row(file_path, is_gzipped):
+def get_header_row(file_path, is_gzipped=True):
     # right now we assume there is only one header row and header row should not be started with '#
     count = 0
+    # right now we only get the header row from tabular files. The tabular files should be always gzipped.
     open_func = gzip.open if is_gzipped else open
     with open_func(file_path, 'rt', encoding='utf-8') as f:
         for line in f:
@@ -379,27 +380,32 @@ def fasta_check(file_path, is_gzipped, info=FASTA_VALIDATION_INFO):
     return error
 
 
-def tabular_file_check(file_format, content_type, file_path, is_gzipped, schemas=TABULAR_FILE_SCHEMAS, max_error=MAX_NUM_ERROR_FOR_TABULAR_FILE, allow_additional_fields=True, schema_path=None):
+def tabular_file_check(file_format, content_type, file_path, schemas=TABULAR_FILE_SCHEMAS, max_error=MAX_NUM_ERROR_FOR_TABULAR_FILE, allow_additional_fields=True, schema_path=None):
     system.trusted = True
     error = {}
     if content_type not in NO_HEADER_CONTENT_TYPE:
-        header_row = get_header_row(file_path, is_gzipped)
+        header_row = get_header_row(file_path)
         dialect = Dialect(comment_char='#', header_rows=[header_row])
     else:
         dialect = Dialect(header=False, comment_char='#')
 
+    # When file is gzipped but filename lacks .gz, frictionless won't auto-detect compression.
+    # Pass compression='gz' so Resource knows to decompress if the file is gzipped.
+    # Checkfiles check if the tabular file is gzipped before calling this function. So we can safely assume the file is gzipped.
+    frictionless_options = {'dialect': dialect,
+                            'format': file_format, 'compression': 'gz'}
     if not schema_path:
         schema_path = schemas.get(content_type)
     if not schema_path:
         # if no schema, we can ignore type-error
         report = validate(file_path, limit_errors=max_error,
-                          skip_errors=['type-error'], dialect=dialect, format=file_format)
+                          skip_errors=['type-error'], **frictionless_options)
     else:
         checks = []
         # handle barcode to sample mapping separately
         if content_type == 'barcode to sample mapping':
             infer_schema = describe(
-                file_path, type='schema', dialect=dialect, format=file_format)
+                file_path, type='schema', **frictionless_options)
             if len(infer_schema.fields) not in [6, 3]:
                 error = {
                     'tabular_file_error': f'barcode to sample mapping file should have 6 or 3 columns, but found {len(infer_schema.fields)} columns'
@@ -410,7 +416,7 @@ def tabular_file_check(file_format, content_type, file_path, is_gzipped, schemas
             else:
                 schema_path = schema_path[1]
             report = validate(file_path, schema=schema_path,
-                              limit_errors=max_error, checks=checks, dialect=dialect, format=file_format)
+                              limit_errors=max_error, checks=checks, **frictionless_options)
         else:
 
             if content_type in ['guide RNA sequences', 'prime editing guide RNA sequences']:
@@ -418,16 +424,16 @@ def tabular_file_check(file_format, content_type, file_path, is_gzipped, schemas
 
             if not allow_additional_fields:
                 report = validate(file_path, schema=schema_path,
-                                  limit_errors=max_error, checks=checks, dialect=dialect, format=file_format)
+                                  limit_errors=max_error, checks=checks, **frictionless_options)
             else:
                 infer_schema = describe(
-                    file_path, type='schema', dialect=dialect, format=file_format)
+                    file_path, type='schema', **frictionless_options)
                 schema = Schema.from_descriptor(schema_path)
                 if len(infer_schema.fields) > len(schema.fields):
                     for i in range(len(schema.fields), len(infer_schema.fields)):
                         schema.add_field(infer_schema.fields[i])
                 report = validate(file_path, schema=schema,
-                                  limit_errors=max_error, checks=checks, dialect=dialect, format=file_format)
+                                  limit_errors=max_error, checks=checks, **frictionless_options)
 
     if not report.valid:
         report = report.flatten(
