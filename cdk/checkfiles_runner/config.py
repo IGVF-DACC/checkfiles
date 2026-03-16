2f92f7bee162d729abe1bbda3cfec06ad3d4361e
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2026-03-16 15:25:52 -0500

    v43-staging-v41-prod (#250)

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -14,6 +14,6 @@ config = {
     'instance_security_group_production': 'sg-0136bee948bf49fe7',
     'backend_uri_sandbox': 'https://api.staging.igvf.org',
     'backend_uri_production': 'https://api.data.igvf.org',
-    'checkfiles_tag_sandbox': 'v42',
+    'checkfiles_tag_sandbox': 'v43',
     'checkfiles_tag_production': 'v41',
 }
